3754b74910f39f7d57789f4ee2ef698b7523d230
marcobonici <bonici.marco@gmail.com>
2022-05-10 00:18:28 +0200

    Modifying Effort to move towards differentiability

--- src/EffortLCDM.jl
+++ src/EffortLCDM.jl
@@ -11,42 +11,36 @@ abstract type AbstractP_lEmulators end
     NN
     InMinMax::Matrix{Float64} = zeros(7,2)
     OutMinMax::Array{Float64} = zeros(3,2)
-    InputParams::Matrix{Float64} = zeros(7,3)
 end
 
 @kwdef mutable struct EmulatorP11_2 <: AbstractComponentEmulators
     NN
     InMinMax::Matrix{Float64} = zeros(7,2)
     OutMinMax::Array{Float64} = zeros(3,2)
-    InputParams::Matrix{Float64} = zeros(7,3)
 end
 
 @kwdef mutable struct EmulatorPloop_0 <: AbstractComponentEmulators
     NN
     InMinMax::Matrix{Float64} = zeros(7,2)
     OutMinMax::Array{Float64} = zeros(12,2)
-    InputParams::Matrix{Float64} = zeros(7,12)
 end
 
 @kwdef mutable struct EmulatorPloop_2 <: AbstractComponentEmulators
     NN
     InMinMax::Matrix{Float64} = zeros(7,2)
     OutMinMax::Array{Float64} = zeros(12,2)
-    InputParams::Matrix{Float64} = zeros(7,12)
 end
 
 @kwdef mutable struct EmulatorPct_0 <: AbstractComponentEmulators
     NN
     InMinMax::Matrix{Float64} = zeros(7,2)
     OutMinMax::Array{Float64} = zeros(12,2)
-    InputParams::Matrix{Float64} = zeros(7,6)
 end
 
 @kwdef mutable struct EmulatorPct_2 <: AbstractComponentEmulators
     NN
     InMinMax::Matrix{Float64} = zeros(7,2)
     OutMinMax::Array{Float64} = zeros(12,2)
-    InputParams::Matrix{Float64} = zeros(7,6)
 end
 
 @kwdef mutable struct EmulatorP_0 <: AbstractP_lEmulators
@@ -66,82 +60,68 @@ function SetkGrid!(cosmoemu::AbstractComponentEmulators, kgrid::Array{T}) where 
 end
 
 function ComputeP_ℓ(cosmoemu::EmulatorP_0, kgrid::Array{T}, bs, f, cosmology) where T
+    input_NN = SetInputNN(cosmoemu.P11_0, kgrid, cosmology)
 
-    P11_comp_array = ComputePk(cosmoemu.P11_0, cosmology, kgrid)
-    Ploop_comp_array = ComputePk(cosmoemu.Ploop_0, cosmology, kgrid)
-    Pct_comp_array = ComputePk(cosmoemu.Pct_0, cosmology, kgrid)
-    
-    b1, b2, b3, b4, b5, b6, b7 = bs
-    b11 = Array([ b1^2, 2*b1*f, f^2 ])
-    bloop = Array([ 1., b1, b2, b3, b4, b1*b1, b1*b2, b1*b3, b1*b4, b2*b2, b2*b4, b4*b4 ])
-    bct = Array([ 2*b1*b5, 2*b1*b6, 2*b1*b7, 2*f*b5, 2*f*b6, 2*f*b7 ])
-    
-    P11_array = zeros(length(kgrid))
-    Ploop_array = zeros(length(kgrid))
-    Pct_array = zeros(length(kgrid))
-    
+    P11_comp_array = ComputePk(cosmoemu.P11_0, input_NN)
+    Ploop_comp_array = ComputePk(cosmoemu.Ploop_0, input_NN)
+    Pct_comp_array = ComputePk(cosmoemu.Pct_0, input_NN)
+
     return SumMultipoleComponents(P11_comp_array, Ploop_comp_array, Pct_comp_array, bs, f)
 end
-
-
 
 function ComputeP_ℓ(cosmoemu::EmulatorP_2, kgrid::Array{T}, bs, f, cosmology) where T
+    input_NN = SetInputNN(cosmoemu.P11_2, kgrid, cosmology)
 
-    P11_comp_array = ComputePk(cosmoemu.P11_2, cosmology, kgrid)
-    Ploop_comp_array = ComputePk(cosmoemu.Ploop_2, cosmology, kgrid)
-    Pct_comp_array = ComputePk(cosmoemu.Pct_2, cosmology, kgrid)
-    
-    b1, b2, b3, b4, b5, b6, b7 = bs
-    b11 = Array([ b1^2, 2*b1*f, f^2 ])
-    bloop = Array([ 1., b1, b2, b3, b4, b1*b1, b1*b2, b1*b3, b1*b4, b2*b2, b2*b4, b4*b4 ])
-    bct = Array([ 2*b1*b5, 2*b1*b6, 2*b1*b7, 2*f*b5, 2*f*b6, 2*f*b7 ])
-    
-    P11_array = zeros(length(kgrid))
-    Ploop_array = zeros(length(kgrid))
-    Pct_array = zeros(length(kgrid))
-    
+    P11_comp_array = ComputePk(cosmoemu.P11_2, input_NN)
+    Ploop_comp_array = ComputePk(cosmoemu.Ploop_2, input_NN)
+    Pct_comp_array = ComputePk(cosmoemu.Pct_2, input_NN)
+
     return SumMultipoleComponents(P11_comp_array, Ploop_comp_array, Pct_comp_array, bs, f)
 end
 
-function SumMultipoleComponents(P11_comp_array, Ploop_comp_array, Pct_comp_array, bs, f)
+function SumMultipoleComponents(P11_comp_array, Ploop_comp_array, Pct_comp_array,
+    bs::Array{T}, f) where T
     b1, b2, b3, b4, b5, b6, b7 = bs
-    
+
     b11 = Array([ b1^2, 2*b1*f, f^2])
     bloop = Array([ 1., b1, b2, b3, b4, b1*b1, b1*b2, b1*b3, b1*b4, b2*b2, b2*b4, b4*b4 ])
     bct = Array([ 2*b1*b5, 2*b1*b6, 2*b1*b7, 2*f*b5, 2*f*b6, 2*f*b7 ])
-    
-    P11_array = zeros(length(P11_comp_array[1,:]))
-    Ploop_array = zeros(length(P11_comp_array[1,:]))
-    Pct_array = zeros(length(P11_comp_array[1,:]))
-    
-    EvaluateP11_0!(P11_array, b11, P11_comp_array)
-    EvaluatePloop_0!(Ploop_array, bloop, Ploop_comp_array)
-    EvaluatePct_0!(Pct_array, bct, Pct_comp_array)
+
+    P11_array = Array{T}(zeros(length(P11_comp_array[1,:])))
+    Ploop_array = Array{T}(zeros(length(P11_comp_array[1,:])))
+    Pct_array = Array{T}(zeros(length(P11_comp_array[1,:])))
+
+    EvaluateP11!(P11_array, b11, P11_comp_array)
+    EvaluatePloop!(Ploop_array, bloop, Ploop_comp_array)
+    EvaluatePct!(Pct_array, bct, Pct_comp_array)
     P_0 = P11_array .+ Ploop_array .+ Pct_array
-    
+
     return P_0
 end
 
-function SetInputNN!(cosmoemu, kgrid::Array{T}, cosmology) where T
-    cosmoemu.InputParams[1,:] .= (cosmology["ln10A_s"] - cosmoemu.InMinMax[1,1])/
+function SetInputNN(cosmoemu, kgrid::Array{T}, cosmology::Array{R}) where {T, R}
+    # assume the parameters are order as ln10A_s, ns, h, omb, omc
+    input_params = Array{R}(zeros(6, length(kgrid)))
+
+    input_params[1,:] .= (cosmology[1] - cosmoemu.InMinMax[1,1])/
     (cosmoemu.InMinMax[1,2]-cosmoemu.InMinMax[1,1])
-    cosmoemu.InputParams[2,:] .= (cosmology["n_s"] - cosmoemu.InMinMax[2,1])/
+    input_params[2,:] .= (cosmology[2] - cosmoemu.InMinMax[2,1])/
     (cosmoemu.InMinMax[2,2]-cosmoemu.InMinMax[2,1])
-    cosmoemu.InputParams[3,:] .= (cosmology["h"] - cosmoemu.InMinMax[3,1])/
+    input_params[3,:] .= (cosmology[3] - cosmoemu.InMinMax[3,1])/
     (cosmoemu.InMinMax[3,2]-cosmoemu.InMinMax[3,1])
-    cosmoemu.InputParams[4,:] .= (cosmology["omega_b"] - cosmoemu.InMinMax[4,1])/
+    input_params[4,:] .= (cosmology[4] - cosmoemu.InMinMax[4,1])/
     (cosmoemu.InMinMax[4,2]-cosmoemu.InMinMax[4,1])
-    cosmoemu.InputParams[5,:] .= (cosmology["omega_cdm"] - cosmoemu.InMinMax[5,1])/
+    input_params[5,:] .= (cosmology[5] - cosmoemu.InMinMax[5,1])/
     (cosmoemu.InMinMax[5,2]-cosmoemu.InMinMax[5,1])
-    cosmoemu.InputParams[6,:]  = (log10.(kgrid) .-cosmoemu.InMinMax[6,1]) ./
+    input_params[6,:]  = (log10.(kgrid) .-cosmoemu.InMinMax[6,1]) ./
     (cosmoemu.InMinMax[6,2]-cosmoemu.InMinMax[6,1])
+
+    return input_params
 end
 
-function ComputePk(cosmoemulator::EmulatorP11_0, cosmology, kgrid::Array{Float64})
-    SetkGrid!(cosmoemulator, kgrid)
-    SetInputNN!(cosmoemulator, kgrid, cosmology)
-    y = cosmoemulator.NN(cosmoemulator.InputParams)
-    n_k = length(kgrid)
+function ComputePk(cosmoemulator::EmulatorP11_0, input_NN)
+    y = cosmoemulator.NN(input_NN)
+    n_k = length(input_NN[1,:])
 
     @avx for i in 1:length(y[:,1])
         for l in 1:n_k
@@ -150,29 +130,29 @@ function ComputePk(cosmoemulator::EmulatorP11_0, cosmology, kgrid::Array{Float64
             y[i,l] = 10^y[i,l]
         end
     end
-    
+
     return y
 end
 
-function ComputePk(cosmoemulator::EmulatorP11_2, cosmology, kgrid::Array{Float64})
-    SetkGrid!(cosmoemulator, kgrid)
-    SetInputNN!(cosmoemulator, kgrid, cosmology)
-    y = cosmoemulator.NN(cosmoemulator.InputParams)
-    n_k = length(kgrid)
+function ComputePk(cosmoemulator::EmulatorP11_2, input_NN)
+    y = cosmoemulator.NN(input_NN)
+    n_k = length(input_NN[1,:])
+
     @avx for i in 1:length(y[:,1])
         for l in 1:n_k
             y[i,l] *= (cosmoemulator.OutMinMax[i,2]-cosmoemulator.OutMinMax[i,1])
             y[i,l] += (cosmoemulator.OutMinMax[i,1])
         end
     end
-    for l in 1:n_k
+
+    @avx for l in 1:n_k
         y[2,l] = 10^y[2,l]
         y[3,l] = 10^y[3,l]
     end
     return y
 end
 
-function EvaluateP11_0!(input_array, b11, Pk_in)
+function EvaluateP11!(input_array, b11, Pk_in)
     @avx for b in 1:3
         for k in 1:length(input_array)
             input_array[k] += b11[b]*Pk_in[b,k]
@@ -180,51 +160,48 @@ function EvaluateP11_0!(input_array, b11, Pk_in)
     end
 end
 
-function ComputePk(cosmoemulator::EmulatorPloop_0, cosmology, kgrid::Array{Float64})
-    SetkGrid!(cosmoemulator, kgrid)
-    SetInputNN!(cosmoemulator, kgrid, cosmology)
-    y = cosmoemulator.NN(cosmoemulator.InputParams)
-    n_k = length(kgrid)
+function ComputePk(cosmoemulator::EmulatorPloop_0, input_NN)
+    y = cosmoemulator.NN(input_NN)
+    n_k = length(input_NN[1,:])
+
     @avx for i in 1:length(y[:,1])
         for l in 1:n_k
             y[i,l] *= (cosmoemulator.OutMinMax[i,2]-cosmoemulator.OutMinMax[i,1])
             y[i,l] += (cosmoemulator.OutMinMax[i,1])
         end
     end
-    
+
     return y
 end
 
-function ComputePk(cosmoemulator::EmulatorPloop_2, cosmology, kgrid::Array{Float64})
-    SetkGrid!(cosmoemulator, kgrid)
-    SetInputNN!(cosmoemulator, kgrid, cosmology)
-    y = cosmoemulator.NN(cosmoemulator.InputParams)
-    n_k = length(kgrid)
+function ComputePk(cosmoemulator::EmulatorPloop_2, input_NN)
+    y = cosmoemulator.NN(input_NN)
+    n_k = length(input_NN[1,:])
+
     @avx for i in 1:length(y[:,1])
         for l in 1:n_k
             y[i,l] *= (cosmoemulator.OutMinMax[i,2]-cosmoemulator.OutMinMax[i,1])
             y[i,l] += (cosmoemulator.OutMinMax[i,1])
         end
     end
-    
+
     return y
 end
 
 
-function EvaluatePloop_0!(input_array, bloop, Pk_in)
+function EvaluatePloop!(input_array, bloop, Pk_in)
     @avx for b in 1:12
         for k in 1:length(input_array)
             input_array[k] += bloop[b]*Pk_in[b,k]
         end
     end
-    
+
 end
 
-function ComputePk(cosmoemulator::EmulatorPct_0, cosmology, kgrid::Array{Float64})
-    SetkGrid!(cosmoemulator, kgrid)
-    SetInputNN!(cosmoemulator, kgrid, cosmology)
-    y = cosmoemulator.NN(cosmoemulator.InputParams)
-    n_k = length(kgrid)
+function ComputePk(cosmoemulator::EmulatorPct_0, input_NN)
+    y = cosmoemulator.NN(input_NN)
+    n_k = length(input_NN[1,:])
+
     @avx for i in 1:length(y[:,1])
         for l in 1:n_k
             y[i,l] *= (cosmoemulator.OutMinMax[i,2]-cosmoemulator.OutMinMax[i,1])
@@ -232,32 +209,31 @@ function ComputePk(cosmoemulator::EmulatorPct_0, cosmology, kgrid::Array{Float64
             y[i,l] = 10^y[i,l]
         end
     end
-    
+
     return y
 end
 
-function ComputePk(cosmoemulator::EmulatorPct_2, cosmology, kgrid::Array{Float64})
-    SetkGrid!(cosmoemulator, kgrid)
-    SetInputNN!(cosmoemulator, kgrid, cosmology)
-    y = cosmoemulator.NN(cosmoemulator.InputParams)
-    n_k = length(kgrid)
+function ComputePk(cosmoemulator::EmulatorPct_2, input_NN)
+    y = cosmoemulator.NN(input_NN)
+    n_k = length(input_NN[1,:])
+
     @avx for i in 1:length(y[:,1])
         for l in 1:n_k
             y[i,l] *= (cosmoemulator.OutMinMax[i,2]-cosmoemulator.OutMinMax[i,1])
             y[i,l] += (cosmoemulator.OutMinMax[i,1])
         end
     end
-    
+
     return y
 end
 
-function EvaluatePct_0!(input_array, bloop, Pk_in)
+function EvaluatePct!(input_array, bloop, Pk_in)
     @avx for b in 1:6
         for k in 1:length(input_array)
             input_array[k] += bloop[b]*Pk_in[b,k]
         end
     end
-    
+
 end
 
 
